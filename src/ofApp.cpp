6d8c8a927f5fe2ac7d3025bfc785e74a3a0fbd91
level-kayak <level-kayak@users.noreply.github.com>
2017-03-30 14:48:09 -0400

    Added random number file

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,8 +3,8 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+X = {500, 1000, 30, 100, 300, 200, 750, 380,45, 1200, 550, 235, 122, 22, 325, 16, 723, 965, 1100, 670};// set of x-coordinates for jewel
+Y = {630, 400, 900, 40, 200, 0, 90, 30,645, 400, 80, 6, 14, 225, 800, 965, 67, 325, 72, 1}; //set of y-coordinates for jewel
 }
 
 //--------------------------------------------------------------
